a9e9bfd09ea5194ab775e8afbad2e4187d8df087
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-12-20 10:39:11 -0300

    tmp: saving progres

---
+++ src/shared/components/Summary/service/sumarry.dto.ts
@@ -0,0 +1,17 @@
+type FindTransactionInputDto = {
+    search?: string;
+    user_id?: number;
+  };
+  
+  type TransactionOutputDto = {
+    id: number;
+    user_id: number;
+    title: string;
+    amount: number;
+    category: string;
+    type: string;
+    created_at: Date;
+  };
+  
+  export type { TransactionOutputDto, FindTransactionInputDto };
+  
